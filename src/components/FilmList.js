84439307d5d57b59d106f6abdaf0db6624ed2d19
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-01-27 18:34:42 +0000

    adds FilmList

---
+++ src/components/FilmList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class FilmList extends Component {
+    render() {
+        return (
+            <h2>Film List</h2>
+        )
+    }
+}
+
+export default FilmList;
--- src/containers/FilmContainer.js
+++ src/containers/FilmContainer.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
+import FilmList from '../components/FilmList';
 
 class FilmContainer extends Component {
     render() {
         return(
+            <div className="film-container">
             <h1>FilmContainer</h1>
+            <FilmList/>
+            </div>
         )
     }
 
