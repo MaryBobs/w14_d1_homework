4556b6dd99756ef0c76ee51280d1879f46d1b677
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-01-27 18:30:31 +0000

    adds FilmContainer

---
+++ src/containers/FilmContainer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FilmContainer extends Component {
+    render() {
+        return(
+            <h1>FilmContainer</h1>
+        )
+    }
+
+
+}
+
+export default FilmContainer;
